6d905cd17f8e9e88369b9687e926a09a98aef3a4
Benjamin Sixel <sixel004@serenity.morris.umn.edu>
2017-11-01 16:38:33 -0500

    Worked on adding total list

--- express.js
+++ express.js
@@ -8,28 +8,48 @@ credentials.host='ids.morris.umn.edu'; //setup database credentials
 
 var connection = mysql.createConnection(credentials); // setup the connection
 
-connection.connect(function(err){if(err){console.log(error)}});
+connection.connect(function(err){
+  if(err){
+    console.log(error)
+  }
+});
 
 app.use(express.static(__dirname + '/public'));
 app.get("/buttons",function(req,res){
-  var sql = 'SELECT * FROM test.till_buttons';
-  connection.query(sql,(function(res){return function(err,rows,fields){
-     if(err){console.log("We have an error:");
+  var sql = 'SELECT * FROM sixel004.till_buttons';
+  connection.query(sql, (function(res){return function(err,rows,fields){
+     if(err){console.log("Houston we have an problem: ");
              console.log(err);}
      res.send(rows);
   }})(res));
 });
+
+app.get("/current",function(req,res){
+  var sql = 'SELECT inventory.item, current_transaction.quantity, prices.price from sixel004.inventory, sixel004.current_transaction, sixel004.prices where (inventory.id = current_transaction.invID AND inventory.id=prices.id)';
+  connection.query(sql, (function(res){return function(err,rows,fields){
+     if(err){console.log("Houston we have an problem: ");
+             console.log(err);}
+     res.send(rows);
+  }})(res));
+});
+
 app.get("/click",function(req,res){
+  var resp = {};
   var id = req.param('id');
-  var sql = 'YOUR SQL HERE'
-  console.log("Attempting sql ->"+sql+"<-");
+  var sql = 'INSERT INTO sixel004.current_transaction (invID, quantity) VALUES ( ' + id + ', 1) ON DUPLICATE KEY UPDATE invID = ' + id + ', quantity = quantity+1';
+  console.log("Attempting sql ->" + sql + "<-");
 
   connection.query(sql,(function(res){return function(err,rows,fields){
-     if(err){console.log("We have an insertion error:");
-             console.log(err);}
+     if(err){
+       resp.err = err;
+       console.log("Houston we have an problem (again): ");
+       console.log(err);
+     }
      res.send(err); // Let the upstream guy know how it went
   }})(res));
 });
 // Your other API handlers go here!
 
 app.listen(port);
+
+console.log("Server started on localhost:"+port);
--- public/click.js
+++ public/click.js
@@ -1,38 +1,53 @@
 angular.module('buttons',[])
-  .controller('buttonCtrl',ButtonCtrl)
-  .factory('buttonApi',buttonApi)
-  .constant('apiUrl','http://localhost:1337'); // CHANGED for the lab 2017!
+.controller('buttonCtrl',ButtonCtrl)
+.factory('buttonApi',buttonApi)
+.constant('apiUrl','http://localhost:1337'); // CHANGED for the lab 2017!
 
 function ButtonCtrl($scope,buttonApi){
-   $scope.buttons=[]; //Initially all was still
-   $scope.errorMessage='';
-   $scope.isLoading=isLoading;
-   $scope.refreshButtons=refreshButtons;
-   $scope.buttonClick=buttonClick;
+  $scope.buttons = []; //Initially all was still
+  $scope.current_list = [];
+  $scope.errorMessage = '';
+  $scope.isLoading = isLoading;
+  $scope.refreshButtons = refreshButtons;
+  $scope.buttonClick = buttonClick;
 
-   var loading = false;
+  var loading = false;
 
-   function isLoading(){
+  function isLoading(){
     return loading;
-   }
+  }
+
+  function totalCost() {
+    return $scope.current_list.reduce(function(sum, item) {
+      return (item.quantity * item.price) + sum;
+    }, 0);
+  }
+
   function refreshButtons(){
     loading=true;
     $scope.errorMessage='';
     buttonApi.getButtons()
-      .success(function(data){
-         $scope.buttons=data;
-         loading=false;
-      })
-      .error(function () {
-          $scope.errorMessage="Unable to load Buttons:  Database request failed";
-          loading=false;
+    .success(function(data){
+      $scope.buttons=data;
+      loading=false;
+    })
+    .error(function () {
+      $scope.errorMessage="Unable to load Buttons:  Database request failed";
+      loading=false;
+    });
+  }
+  function buttonClick($event, buttonID, invID){
+    console.log("Inventory ID for button: " + invID);
+    $scope.errorMessage='';
+    buttonApi.clickButton($event.target.id)
+    .success(function(){
+      buttonApi.getCurrent().success(function(data) {
+        $scope.current_list = data;
+        console.log("Got a list of the current transaction:");
+        console.log(data);
       });
- }
-  function buttonClick($event){
-     $scope.errorMessage='';
-     buttonApi.clickButton($event.target.id)
-        .success(function(){})
-        .error(function(){$scope.errorMessage="Unable click";});
+    })
+    .error(function(){$scope.errorMessage="Unable to complete click";});
   }
   refreshButtons();  //make sure the buttons are loaded
 
@@ -46,9 +61,12 @@ function buttonApi($http,apiUrl){
     },
     clickButton: function(id){
       var url = apiUrl+'/click?id='+id;
-//      console.log("Attempting with "+url);
+      //      console.log("Attempting with "+url);
       return $http.get(url); // Easy enough to do this way
+    },
+    getCurrent: function() {
+      var url = apiUrl + '/current';
+      return $http.get(url);
     }
- };
+  };
 }
-
